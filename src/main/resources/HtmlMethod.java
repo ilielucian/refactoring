fe1e28d1c108cf9795278a03d152b4ed550aea57
ilielucian <ilielucian@outlook.com>
2018-04-30 11:00:47 +0300

    add test for html method, for faster demonstration

--- src/main/resources/HtmlMethod.java
+++ src/main/resources/HtmlMethod.java
@@ -22,4 +22,14 @@ public class HtmlMethod {
 //                "</EM> frequent renter points<P>";
 //        return result;
 //    }
+
+    private static String getExpectedHtmlStatement() {
+        return "<H1>Rentals for <EM>Lucian</EM></ H1><P>\n" +
+                "Godfather 1: 9.5<BR>\n" +
+                "Shawshank Redemption: 9.5<BR>\n" +
+                "Beauty and the beast: 12.0<BR>\n" +
+                "Black Panther: 9.0<BR>\n" +
+                "<P>You owe <EM>40.0</EM><P>\n" +
+                "On this rental you earned <EM>5</EM> frequent renter points<P>";
+    }
 }
